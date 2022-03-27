6fbd1258c540cf4d80861f0ff4a10d501e089b1e
KP <kpbor2014@gmail.com>
2022-03-27 16:32:20 +0300

    day 21 - 7-2 Авторизация пользователей

--- storedvd/urls.py
+++ storedvd/urls.py
@@ -24,7 +24,8 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/',include('shop.urls')),
-    path('',RedirectView.as_view(url='/shop/', permanent=True))
+    path('',RedirectView.as_view(url='/shop/', permanent=True)),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
